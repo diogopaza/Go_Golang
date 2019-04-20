65da466294ce24ce34492eb78345649496fd9e88
diogopaza <diogopazacvel@gmail.com>
2019-04-19 22:54:55 -0300

    returning

--- livro_go/src/conversor.go
+++ livro_go/src/conversor.go
@@ -10,15 +10,30 @@ import(
 
 func main(){
 
+	var unidadeDestino string
+
 	if len(os.Args) < 3 {
 		fmt.Println("Uso: conversor <valores> <unidade>")
 		os.Exit(1)
 	}
 
-	
 	unidadeOrigem:= os.Args[len(os.Args)-1 ]
-	fmt.Println(unidadeOrigem)
 
+	if unidadeOrigem == "celsius"{
+		unidadeDestino = "fahrenheit"
+	}else if unidadeOrigem == "quilometros"{
+		unidadeDestino = "milhas"
+	}else{
+		fmt.Println("%s não é uma unidade conhecida", unidadeDestino)
+		os.Exit(1)
+	}
+
+	/*
+	
+	fmt.Println(unidadeOrigem)
+	valorOrigem:= os.Args[len(os.Args)-2]
+	fmt.Println(valorOrigem)
+	*/
 
 
 
