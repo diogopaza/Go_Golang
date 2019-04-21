8a196241de0a1cdb4c0c576d0fbd5c281cb518af
diogopaza <diogopazacvel@gmail.com>
2019-04-21 16:31:05 -0300

    slice

--- livro_go/src/conversor.go
+++ livro_go/src/conversor.go
@@ -3,21 +3,22 @@ package main
 import(
 
 	"fmt"
-	"os"
-	
+	"os"	
 
 )
 
 func main(){
 
 	var unidadeDestino string
-
+	var valoresOrigem = os.Args[1 : len(os.Args)-1]
+	
 	if len(os.Args) < 3 {
 		fmt.Println("Uso: conversor <valores> <unidade>")
 		os.Exit(1)
 	}
 
-	unidadeOrigem:= os.Args[len(os.Args)-1 ]
+	fmt.Println(valoresOrigem)
+	unidadeOrigem:= os.Args[len(os.Args)-1]
 
 	if unidadeOrigem == "celsius"{
 		unidadeDestino = "fahrenheit"
@@ -28,7 +29,17 @@ func main(){
 		os.Exit(1)
 	}
 
+
+
+
+
 	/*
+	for i, v:= range valoresOrigem{
+
+
+	}
+
+	
 	
 	fmt.Println(unidadeOrigem)
 	valorOrigem:= os.Args[len(os.Args)-2]
