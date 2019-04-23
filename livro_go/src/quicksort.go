362153640e3114056832ee0dd03a74301de1ff49
diogopaza <diogopazacvel@gmail.com>
2019-04-23 15:52:46 -0300

    quicksort with go

--- livro_go/src/quicksort.go
+++ livro_go/src/quicksort.go
@@ -4,20 +4,45 @@ import(
 
 	"fmt"
 	"os"
-	//"strconv"
+	"strconv"
+	
 
 )
 
 func main(){
 
 	var numeros_entrada = os.Args[1:]
+	var numeros = make([]int, len(numeros_entrada))
+	//fmt.Println(numeros)
 	
+	for i, n:= range numeros_entrada{
 
-	for i, v := range(numeros_entrada){
+		numero, err := strconv.Atoi(n)
+		if err != nil{
+			fmt.Printf("%s não é um núemro válido \n", n)
+			os.Exit(1)
+		}
 
-		fmt.Printf("Numero %s no indice %d \n",v,i)
+		numeros[i]=numero
 
-	} 
+	}
 
+	fmt.Println(quicksort(numeros))
+	
+}
+
+
+func quicksort(numeros []int)[]int{
+
+	if len(numeros) <= 1{
+		
+		fmt.Println("array de uma posição ou zero")
+		return numeros
+	}
+
+	n:= make([]int, len(numeros))
+	copy(n, numeros)
+
+	
 }
 
