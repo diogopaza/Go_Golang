097488d513d7dbd6d3f47406edc728eddd0774cf
diogopaza <diogopazacvel@gmail.com>
2019-04-25 17:11:59 -0300

    finished map

--- livro_go/src/maps.go
+++ livro_go/src/maps.go
@@ -13,31 +13,44 @@ func main(){
 	var palavras = os.Args[1:]
 	
 
-	//estatisticas := colherEstatisticas(palavras)
-	//fmt.Println(estatisticas)
-	//imprimir(estatisticas )
-	colherEstatisticas(palavras)
+	estatisticas := colherEstatisticas(palavras)
+	
+	imprimir(estatisticas )
+	
 
 
 }
 
 
-func colherEstatisticas(palavras []string) /*map[string]int*/ {
+func colherEstatisticas(palavras []string) map[string]int {
 
 	estatisticas := make(map[string]int)
 	//fmt.Println(estatisticas)
-	_ = estatisticas
+	
 
 	for _, palavra := range(palavras){
 		var letraInicial = string(palavra[0])
-		fmt.Println(string( letraInicial ))
+		
+		contador, encontrado := estatisticas[letraInicial]
+		if encontrado{
+			estatisticas[letraInicial] = contador + 1
+		}else{
+			estatisticas[letraInicial] = 1
+		}
 	}
+
+	return estatisticas
 
 
 }
 
 
-func imprimir(){
+func imprimir(estatisticas map[string]int){
+	fmt.Println("Contagem de palavras iniciadas em cada letra:")
+	for inicial, contador := range estatisticas{
 
+		fmt.Printf("%s = %d\n", inicial, contador)
+	}
+		
 
 }
