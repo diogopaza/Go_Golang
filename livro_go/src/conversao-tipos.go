a47c7c0355cbd2ceb9e7df4e6e624f4414f81dbb
diogopaza <diogopazacvel@gmail.com>
2019-05-10 16:09:25 -0300

    types

---
+++ livro_go/src/conversao-tipos.go
@@ -0,0 +1,25 @@
+package main
+
+import(
+
+	"fmt"
+
+)
+
+
+type ListaDeCompras []string
+
+func main(){
+
+
+
+    
+}
+
+
+func imprimirSlice(slice []string){
+
+	fmt.Println("Slice: ", slice)
+
+}
+
