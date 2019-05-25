838b090e83395ac4d4914addd6745b527b7fd45e
diogopaza <diogopazacvel@gmail.com>
2019-05-25 11:09:51 -0300

    projeto pg.140

--- livro_go/projeto-encurtador-url/servidor.go
+++ livro_go/projeto-encurtador-url/servidor.go
@@ -6,6 +6,7 @@ import(
 	"net/http"
 	"log"
 	"time"
+	
 )
 
 var(
@@ -17,9 +18,11 @@ var(
 type Url struct{
 
 	Id string
-	Criacao time.time
+	Criacao time.Time
 	Destino string
 }
+
+type Headers map[string]string
 
 func init(){
 	porta = 8888
@@ -27,19 +30,58 @@ func init(){
 
 }
 
-func Encurtador(){
+func encurtador(w http.ResponseWriter, r * http.Request){
+
+	if r.Method != "Get"{
+		responderCom(w, http.StatusMethodNotAllowed, Headers{
+			"Allow":"Post",			
+		})
+		w.Write([]byte("entrei"))
+		minhaUrl := extrairUrl(r)
+		fmt.Printf(minhaUrl)
+		return
+	}
+	
 
 }
 
-func Redirecionar(){
+func redirecionar(w http.ResponseWriter, r * http.Request){
+
+	w.Write([]byte("Estou no redirecionar"))
 
 }
+
+func responderCom(w http.ResponseWriter,
+					status int,
+					headers Headers,){
+						
+						for k, v:= range headers{
+							w.Header().Set(k,v)
+						}
+						
+						w.WriteHeader(status)
+}
+
+
+func extrairUrl(r *http.Request)string{
+
+	url := make([]byte, r.ContentLength, r.ContentLength)
+	r.Body.Read(url)
+	return string(url)
+	
+
+}
+
+
+
+
 
 func main(){
 
-		http.HandleFunc("/api", Encurtador)
-		http.HandleFunc("/api", Redirecionar)
-
+	
+		http.HandleFunc("/encurtar", encurtador)
+		http.HandleFunc("/redirecionar", redirecionar)
+		
 		log.Fatal(http.ListenAndServe(
 			fmt.Sprintf(":%d", porta), nil))
 		
